08676d51f2201a7c9da51e79a1f8beec397c255c
shantanu34 <shantanu34@outlook.com>
2019-01-24 00:56:04 +0530

    check

--- SqlLoader.py
+++ SqlLoader.py
@@ -1,3 +1,6 @@
+import cgi
+
+
 class SqlLoader:
     selectList = list()
     tableName = ""
@@ -7,8 +10,8 @@ class SqlLoader:
         self.tableName = tableName
         return self
 
-    def join(self,tableName,whereText,joinType="join"):
-        if joinType=='join':
+    def join(self, tableName, whereText, joinType="join"):
+        if joinType == 'join':
             print("ohh join")
 
     async def doThread(self, parameter_list):
@@ -18,8 +21,18 @@ class SqlLoader:
         self.selectList.append(value)
         return self
 
-    def where(self,key,value,operator="="):
-        self.whereList.append(key+" "+operator+" '"+value+"'")
+    def where(self, key, value, operator="="):
+        self.whereList.append(key+" "+operator+" '"+cgi.escape(value)+"'")
 
     def get(self):
-        return "select "+("".join(self.selectList))+" from "+self.tableName
+        sql = "select "
+        if not self.selectList:
+            sql += "* "
+        else:
+            sql += ("".join(self.selectList))
+
+        if not self.selectList:
+            sql+="* "
+        else:
+            sql+=("".join(self.selectList))
+        return sql+" from "+self.tableName
