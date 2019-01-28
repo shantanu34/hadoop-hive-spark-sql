c6223b459933d0a99f08ecb37025bd1fd153d827
umbrashia <shantanu34@outlook.com>
2019-01-28 15:30:25 +0530

    working on select and join functionality

--- HadoopHiveSparkSql.py
+++ HadoopHiveSparkSql.py
@@ -1,11 +1,15 @@
 from SqlLoader import SqlLoader
 
+
 class HadoopHiveSparkSql:
-    
+
     @staticmethod
     def table(tableName):
         return SqlLoader().table(tableName)
-    
 
-        
-print(HadoopHiveSparkSql.table("employee").select("id,name").select(",department").get())
+
+print(HadoopHiveSparkSql.table("employee")
+      .select("id,name")
+      .select(",department")
+      .where("ram=(select id from users)")
+      .where("city=", "delhi").get())
--- SqlLoader.py
+++ SqlLoader.py
@@ -5,14 +5,27 @@ class SqlLoader:
     selectList = list()
     tableName = ""
     whereList = list()
+    sqlLimit = str("")
+    sqlJoinList = list()
 
     def table(self, tableName):
         self.tableName = tableName
         return self
 
-    def join(self, tableName, whereText, joinType="join"):
-        if joinType == 'join':
-            print("ohh join")
+    def join(self, tableName, whereText, joinType="INNER"):
+        joinType = joinType.strip().upper()
+        if joinType == "INNER" or joinType == "JOIN":
+            self.sqlJoinList.append("INNER JOIN "+tableName+" ON "+whereText)
+        if joinType == "LEFT":
+            self.sqlJoinList.append("LEFT JOIN "+tableName+" ON "+whereText)
+        if joinType == "RIGHT":
+            self.sqlJoinList.append("RIGHT JOIN "+tableName+" ON "+whereText)
+        if joinType == "FULL OUTER":
+            self.sqlJoinList.append(
+                "FULL OUTER JOIN "+tableName+" ON "+whereText)
+        if joinType not in ['FULL OUTER', 'INNER', 'LEFT', 'RIGHT', 'JOIN']:
+            self.sqlJoinList.append(joinType+" "+tableName+" ON "+whereText)
+        return self
 
     async def doThread(self, parameter_list):
         print("Jai Ho")
@@ -21,18 +34,29 @@ class SqlLoader:
         self.selectList.append(value)
         return self
 
-    def where(self, key, value, operator="="):
-        self.whereList.append(key+" "+operator+" '"+cgi.escape(value)+"'")
+    def where(self, keyOrRaw, value=""):
+        if not value:
+            self.whereList.append(keyOrRaw)
+        else:
+            self.whereList.append(keyOrRaw+"'"+cgi.escape(value)+"'")
+        return self
+
+    def limit(self, limitValue, offset=""):
+        if not offset:
+            self.sqlLimit = " LIMIT "+limitValue
+        else:
+            self.sqlLimit = " LIMIT "+offset+","+limitValue
+        return self
 
     def get(self):
-        sql = "select "
+        sql = "SELECT "
         if not self.selectList:
             sql += "* "
         else:
             sql += ("".join(self.selectList)).strip()
 
-        if not self.selectList:
-            sql+="* "
-        else:
-            sql+=("".join(self.selectList)).strip()
-        return sql+" from "+self.tableName
+        sql += " FROM "+self.tableName
+
+        if self.whereList:
+            sql += " WHERE "+(" AND ".join(self.whereList)).strip()
+        return sql
